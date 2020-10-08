898b955ac5b6de6d6cc1e63f4e66081f4cab2d4c
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-08 20:49:45 +0530

    new pattern
    fixed all issues with the github and ecllipse commit, now i only have to write the code and throw it

--- introduction/src/module-info.java
+++ introduction/src/module-info.java
@@ -1,2 +1,9 @@
 module introduction {
+	exports conditionalStatement;
+	exports dataTypes;
+	exports operators;
+	exports typeCasting;
+	exports loops;
+	exports userInput;
+	exports introduction;
 }
