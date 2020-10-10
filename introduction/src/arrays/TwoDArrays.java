fc59243b666f3cd4dba4c2d39ca3f5146977b0be
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-10 19:13:06 +0530

    Dave 2D Arrays

---
+++ introduction/src/arrays/TwoDArrays.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class TwoDArrays {
+
+	public static void main(String[] args) {
+		
+		int a[][] = new int [5][3];
+		
+		int b[][] = {{3 , 1, 5, 4,}, {4, 5, 2, 4,}};
+		
+		System.out.println(b[1][1]);
+
+	}
+
+}
