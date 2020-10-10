f7ad071a9f51c2a76e10dd6b16db742122f9490f
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-11 00:29:42 +0530

    Update BubbleSort.java
    some optimisation in the bubble sort

--- introduction/src/arrays/BubbleSort.java
+++ introduction/src/arrays/BubbleSort.java
@@ -6,14 +6,21 @@ public class BubbleSort {
 		int a[] = {2,1,8,-3,5,3};
 		int n = a.length;
 		for(int i = 0; i < n-1; i++) {
-			for(int j= 0; j < n-1; j++) {
+			
+			boolean sorted = true;
+			
+			for(int j= 0; j < n-1-i; j++) {
 				if(a[j+1] < a[j]) {
 					int temp = a[j+1];
 					a[j+1] = a[j];
 					a[j] = temp;
+					
+					sorted = false;
 				}
 			}
+			if(sorted) break;
 		}
+		//for each loop
 		for(int item: a) {
 			System.out.print(item + " ");
 		}
