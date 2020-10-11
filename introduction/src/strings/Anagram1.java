5f1212357780e42739306a7ca7ec43eef7754ae4
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-12 00:49:07 +0530

    Create Anagram1.java

---
+++ introduction/src/strings/Anagram1.java
@@ -0,0 +1,36 @@
+package strings;
+
+public class Anagram1 {
+	public static void main(String[] args) {
+		String a = "enola";
+		String b = "alone";
+		
+		boolean isAnagram = true;
+		
+		int al[] = new int[256];
+		int bl[] = new int[256];
+		
+		for(char c: a.toCharArray()) {
+			int index = (int) c;
+			al[index]++;
+		}
+		for(char c: b.toCharArray()) {
+			int index = (int) c;
+			bl[index]++;
+		}
+		for(int i = 0; i<256; i++) {
+			if(al[i] !=bl[i]) {
+				isAnagram = false;
+				break;
+			}
+		}
+
+		if(isAnagram) {
+			System.out.println("Anagram");
+		}else {
+			System.out.println("Not Anagram");
+		}
+		
+	}
+
+}
