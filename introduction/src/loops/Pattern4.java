ea97bbea8123526660ce5b5724f6cb61bf6404de
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-13 19:30:07 +0530

    Pattern4
    updated with better look

--- introduction/src/loops/Pattern4.java
+++ introduction/src/loops/Pattern4.java
@@ -13,7 +13,7 @@ public class Pattern4 {
 				System.out.print("  ");
 			}
 			for(int j = 1; j <= i; j++) {
-				System.out.print("*  ");
+				System.out.print("*   ");
 			}
 			System.out.println();
 		}
