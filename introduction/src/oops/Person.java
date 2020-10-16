d02425472b04d2350a5866f91d30daee402921a4
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-16 22:39:41 +0530

    static

---
+++ introduction/src/oops/Person.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ introduction/src/oops/StaticKeyword.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class StaticKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
