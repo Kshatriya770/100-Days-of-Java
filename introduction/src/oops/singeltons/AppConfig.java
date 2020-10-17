7c3aca81a5d74e5abe81290e33a5e6a77c22234d
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-18 02:09:41 +0530

    singelton update

--- introduction/src/oops/singeltons/AppConfig.java
+++ introduction/src/oops/singeltons/AppConfig.java
@@ -5,9 +5,12 @@ public class AppConfig {
 	private AppConfig() {
 		
 	}
-	private static AppConfig obj = new AppConfig();
+	private static AppConfig obj = null;
 	
 	public static AppConfig getInstance() {
+		if(obj == null) {
+			obj = new AppConfig();
+		}
 		return obj;
 		
 	}
