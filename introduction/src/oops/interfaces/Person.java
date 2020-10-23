cdcd2e179ef005083277031a247955b656c4af38
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-23 23:52:40 +0530

    class file updated

--- introduction/src/oops/interfaces/Person.java
+++ introduction/src/oops/interfaces/Person.java
@@ -1,37 +1,37 @@
 package oops.interfaces;
 
-public abstract class Person implements Student, Youtuber{
+public class Person implements Student, Youtuber{
 
 	public static void main(String[] args) {
 		
-		//Person obj = new Person();
-		//obj.study();
-		//obj.makeVideo();
+		Person obj = new Person();
+		obj.study();
+		obj.makeVideo();
 		
-		// Youtuber obj1 = obj;
-		// obj1.editVideo();
-		// obj1.makeVideo();
+		 Youtuber obj1 = obj;
+		 obj1.editVideo();
+		 obj1.makeVideo();
 		
 
 	}
 
-	// @Override
-	// public
-	// void study() {
-	// 	System.out.println("Person is studying");
+	@Override
+	public
+	void study() {
+		System.out.println("Person is studying");
 		
-	// }
+	}
 
-	// @Override
-	// public void makeVideo() {
-	// 	System.out.println("person is making a good video");
+	@Override
+	public void makeVideo() {
+		System.out.println("person is making a good video");
 		
-	// }
+	}
 
-	// @Override
-	// public void editVideo() {
-	// 	// TODO Auto-generated method stub
+	@Override
+	public void editVideo() {
+		// TODO Auto-generated method stub
 		
-	// }
+	}
 
 }
--- introduction/src/oops/interfaces/Youtuber.java
+++ introduction/src/oops/interfaces/Youtuber.java
@@ -5,5 +5,11 @@ import oops.abstraction.VideoEditor;
 public abstract interface Youtuber extends VideoEditor {
 	
 	abstract void makeVideo();
+	
+	
+	//added after java 8
+	default void uploadVideo() {
+		System.out.println("upload the video");
+	}
 
 }
