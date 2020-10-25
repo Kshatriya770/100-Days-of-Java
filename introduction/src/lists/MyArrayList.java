96ccb9297d673d641f5bf4c0bbe9f05fc4051e9f
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-25 13:51:30 +0530

    ArrayList examples
    some more methods in the same file

--- introduction/src/lists/MyArrayList.java
+++ introduction/src/lists/MyArrayList.java
@@ -1,16 +1,54 @@
 package lists;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MyArrayList {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> fruits = new ArrayList();
+		//ArrayList<String> fruits = new ArrayList();    or
+		List<String> fruits = new ArrayList();
+		List<String> vegetables = new ArrayList();
 		
 		fruits.add("Apple");
 		fruits.add("Orange");
+		
+		vegetables.add("bhindi");
+		vegetables.add("satputiya");
+	
+		fruits.addAll(vegetables);
+		
+		String temp[] = new String[fruits.size()];
+		fruits.toArray(temp);
+		for(String e: temp) {
+			System.out.println(e);
+		}
+		
+		System.out.println(fruits.get(3));
+		
+		fruits.set(3, "ghewda");
+		fruits.remove(2);
+		
+		
 		System.out.println(fruits);
+		
+		System.out.println(fruits.contains("Apple"));
+		System.out.println(fruits.contains("mango"));
+		
+		//to remove list
+		List<String> toRemove = new ArrayList();
+		toRemove.add("Apple");
+		toRemove.add("Orange");
+		fruits.removeAll(toRemove);
+		System.out.println(fruits);
+		
+		System.out.println(fruits.size());
+		
+		fruits.clear();
+		System.out.println(fruits);
+		System.out.println(fruits.isEmpty());
+		
 		
 		ArrayList<Integer> marks = new ArrayList();
 		
