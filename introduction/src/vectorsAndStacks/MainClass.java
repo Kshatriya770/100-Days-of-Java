02e96b5ff903ddfdd645978b40747ad6af08a347
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-29 00:33:09 +0530

    linked list

---
+++ introduction/src/vectorsAndStacks/MainClass.java
@@ -0,0 +1,16 @@
+package vectorsAndStacks;
+
+import java.util.Vector;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+
+		Vector<Integer> v = new Vector();
+		
+		//same as array list but this is not efficient so we don't use this instead of Array list
+
+	}
+
+}
