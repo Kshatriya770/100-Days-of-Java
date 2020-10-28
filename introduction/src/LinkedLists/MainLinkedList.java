10d795954e6061c0871c91a774682da69c098f7f
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-28 07:20:37 +0530

    created new branch and made some optimization in linked list

--- introduction/src/LinkedLists/MainLinkedList.java
+++ introduction/src/LinkedLists/MainLinkedList.java
@@ -8,35 +8,15 @@ public class MainLinkedList {
 
 	public static void main(String[] args) {
 		
-		List<Integer> ll = new LinkedList<>();
-		List<Integer> al = new ArrayList<>();
+		MyLinkedList myLL = new MyLinkedList();
 		
-		getTimeDiff(al);
-		getTimeDiff(ll);
-//		ll.add(12);
-//		ll.add(2);
-//		ll.add(32);
-//		
-//		System.out.println(ll);
-//		System.out.println(ll.get(2));
-//		
-//		ll.toArray();
-//		System.out.println(ll);
-		
-		System.out.println(ll.get(1));
-
-	}
-	
-	static void getTimeDiff(List<Integer> list) {
-		
-		long start = System.currentTimeMillis();
-		
-		for(int i = 0 ; i < 10000000; i++) {
-			list.add(i);
+		for(int i = 0; i<10; i++) {
+			myLL.add(i);
 		}
 		
-		long end = System.currentTimeMillis();
-		System.out.println(list.getClass().getName() + " --> " +(end - start));
-	}
+		myLL.print();
+		
+		
+}
 
 }
--- introduction/src/LinkedLists/MyLinkedList.java
+++ introduction/src/LinkedLists/MyLinkedList.java
@@ -1,5 +1,48 @@
 package LinkedLists;
 
 public class MyLinkedList {
+	
+	Node head;
+	void add(int data) {
+		Node toAdd = new Node(data);
+		
+		if(isEmpty()) {
+			head = toAdd;
+			return;
+		}
+		
+		Node temp = head;
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = toAdd;
+	}
+	
+	void print() {
+		Node temp = head;
+		while(temp.next != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+	
+	boolean isEmpty() {
+		if(head == null) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+	static class Node{
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
 
 }
