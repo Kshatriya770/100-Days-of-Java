21037b64abfd3c39294b91b6a95a47c08f3290a9
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-11-01 19:04:40 +0530

    more

--- introduction/src/gfgcoding/SubarrayWithZeroSum.java
+++ introduction/src/gfgcoding/SubarrayWithZeroSum.java
@@ -12,6 +12,7 @@ public class SubarrayWithZeroSum {
 	public static void main(String[] args) {
 		
 		int[] a = {2, 1, -3, 4, 2};
+		int k = 6;
 		boolean found = false;
 		
 //		for(int i = 0; i < a.length; i++) {
@@ -25,13 +26,15 @@ public class SubarrayWithZeroSum {
 //			}
 //			if(found) break;
 //		}
+//
+//with the help of the set :::
 		
 		Set<Integer> set = new HashSet<>();
 		int sum = 0;
 		for(int element : a) {
 			set.add(sum);
 			sum += element;
-			if(set.contains(sum)) {
+			if(set.contains(sum -k)) {
 				found = true;
 				break;
 			}
