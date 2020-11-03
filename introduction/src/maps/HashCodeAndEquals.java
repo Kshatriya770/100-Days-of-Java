5e8d7d7747e7c2e41a5553ba1588e7eab6b6a656
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-11-03 19:38:57 +0530

    generating hash code with ecllipse

--- introduction/src/maps/HashCodeAndEquals.java
+++ introduction/src/maps/HashCodeAndEquals.java
@@ -31,12 +31,33 @@ class Pen{
 	public Pen(int price, String color) {
 		this.price = price;
 		this.color = color;
+		
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
-		Pen that = (Pen) obj;
-		boolean isEqual = this.price == that.price && this.color.equals(that.color);
-		return isEqual;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pen other = (Pen) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + price;
+		return result;
 	}
 }
