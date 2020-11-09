8e4359e4c9c6952b472378669dbb634909bdc681
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-11-09 19:14:26 +0530

    number mirror

---
+++ introduction/src/codechef/NumberMirror.java
@@ -0,0 +1,14 @@
+package codechef;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class NumberMirror
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+	}
+}
+
