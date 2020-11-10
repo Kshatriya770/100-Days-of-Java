6b24f363baf226bbf009560533ba35cae12a2f36
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-11-10 11:58:58 +0530

    just checking

---
+++ introduction/src/codechef/PlayingWithMatches.java
@@ -0,0 +1,27 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class PlayingWithMatches {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int t1 = 0;
+		int t2 = 0;
+		for(int i = 1; i <= t; i++) {
+			
+			t1 = sc.nextInt();
+			t2 = sc.nextInt();
+			int sum = t1 + t2;
+			int length = String.valueOf(sum).length();
+			for(int j = 1; j<= length; j++) {
+				System.out.println(sum);
+			}
+			System.out.println(length);
+		}
+
+	}
+
+}
