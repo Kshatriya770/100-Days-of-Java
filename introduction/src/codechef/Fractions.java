7ee168bdf45a9258edbf1eeafa82960d3fc28135
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-11-29 19:31:36 +0530

    some codechef and linked list revision

---
+++ introduction/src/codechef/Fractions.java
@@ -0,0 +1,18 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class Fractions {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int N = x.nextInt();
+		int a[] = new int[N];
+		for(int i = 0; i <N; i++) {
+			a[i] = i+1;
+		}
+		
+
+	}
+
+}
---
+++ introduction/src/linkedlist/MainLinkedList.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+import java.util.*;
+
+public class MainLinkedList {
+
+	public static void main(String[] args) {
+		
+		MyLinkedLIst myLL = new MyLinkedList();
+		myLL.add(12);
+		myLL.add(43);
+		
+	}
+
+}
