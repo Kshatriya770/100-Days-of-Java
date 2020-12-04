0a4ad9b2fb0acea7c3cc52fb9d2d498b31522091
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-12-05 00:00:12 +0530

    codeforces

---
+++ introduction/src/codeforces/CancelTheTrains.java
@@ -0,0 +1,22 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class CancelTheTrains {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int t = x.nextInt();
+		while(t-->0) {
+			int n = x.nextInt();
+			int m = x.nextInt();
+			int a[] = new int[n];
+			for(int i =0; i<n; i++) {
+				a[i] = x.nextInt();
+			}
+			
+		}
+
+	}
+
+}
