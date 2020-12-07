56b0858315b21efd7c9f510c23235fb92c5075e7
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-12-07 15:40:18 +0530

    day 1 vaccine problem codechef

---
+++ introduction/src/codechef/Vaccine.java
@@ -0,0 +1,18 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class Vaccine {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int D1 = x.nextInt();
+		int V1 = x.nextInt();
+		int D2 = x.nextInt();
+		int V2 = x.nextInt();
+		int P = x.nextInt();
+		
+
+	}
+
+}
