b1eb88649435cdf1c7c152527c72842a62bf24f1
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-12-08 10:20:42 +0530

    vaccine problem partially solved

--- introduction/src/codechef/Vaccine.java
+++ introduction/src/codechef/Vaccine.java
@@ -11,8 +11,24 @@ public class Vaccine {
 		int D2 = x.nextInt();
 		int V2 = x.nextInt();
 		int P = x.nextInt();
+		int A,V,D;
 		
-
+		if(D1>D2) {
+			A = D1-D2;
+			V = A*V2;
+			D = D2;
+		}else {
+			A = D2-D1;
+			V = A*V1;
+			D = D1;
+		}
+		
+		while(P > V) {
+			V = V + V1 + V2;
+			D = D + 1;
+		}
+		System.out.println(D-1);
+		x.close();
 	}
 
 }
